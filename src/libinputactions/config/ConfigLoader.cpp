abd5ff19c0fa84b0708bad8ac42f1407635acc63
taj-ny <79316397+taj-ny@users.noreply.github.com>
2026-06-06 13:25:22 +0200

    config: allow empty files

--- src/libinputactions/config/ConfigLoader.cpp
+++ src/libinputactions/config/ConfigLoader.cpp
@@ -92,7 +92,9 @@ bool ConfigLoader::load(const ConfigLoadSettings &settings)
 Config ConfigLoader::createConfig(const QString &raw)
 {
     const auto root = Node::create(raw);
-    if (!root->isMap()) {
+    if (root->isNull()) {
+        return {};
+    } else if (!root->isMap()) {
         throw InvalidNodeTypeConfigException(root.get(), NodeType::Map);
     }
 
