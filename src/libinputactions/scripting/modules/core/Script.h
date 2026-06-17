0e9d377d289af2e118f7c5199c542da9e627276c
taj-ny <79316397+taj-ny@users.noreply.github.com>
2026-06-17 13:59:05 +0200

    scripting/core: add Script object

---
+++ src/libinputactions/scripting/modules/core/Script.h
@@ -0,0 +1,41 @@
+/*
+    Input Actions - Input handler that executes user-defined actions
+    Copyright (C) 2024-2026 Marcin Woźniak
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <QObject>
+
+namespace InputActions
+{
+
+class Script : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString rootDirectory READ rootDirectory)
+
+public:
+    Script(QString rootDirectory);
+
+    const QString &rootDirectory() const { return m_rootDirectory; }
+
+private:
+    QString m_rootDirectory;
+};
+
+}
